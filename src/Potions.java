5043d00096eb6cf88d99e3910d586b82433f921e
Jordanlw <jordanw2@gmail.com>
2014-11-01 20:26:06 +1030

    Changed the way enum values are accessed from within Potions class & some fields changes

--- src/Potions.java
+++ src/Potions.java
@@ -9,18 +9,18 @@ import com.badlogic.gdx.graphics.Texture;
  * To change this template use File | Settings | File Templates.
  */
 class Potions extends Character {
-    static final float timeToReach = 15;
-    static final float healthGiven = 15;
+    static final float secsTillDisappear = 15;
+    static final float healthGiven = 30;
     static final Texture[] textures = new Texture[PotionsTypes.amount()];
     float time;
 
     public static void initializeTextures() {
-        textures[PotionsTypes.BLUE.ordinal()] = new Texture(Gdx.files.internal("resources/blue.png"));
-        textures[PotionsTypes.EMPTY.ordinal()] = new Texture(Gdx.files.internal("resources/empty.png"));
-        textures[PotionsTypes.GREEN.ordinal()] = new Texture(Gdx.files.internal("resources/green.png"));
-        textures[PotionsTypes.PURPLE.ordinal()] = new Texture(Gdx.files.internal("resources/purple.png"));
-        textures[PotionsTypes.RED.ordinal()] = new Texture(Gdx.files.internal("resources/red.png"));
-        textures[PotionsTypes.YELLOW.ordinal()] = new Texture(Gdx.files.internal("resources/yellow.png"));
+        textures[PotionsTypes.BLUE.potion] = new Texture(Gdx.files.internal("resources/blue.png"));
+        textures[PotionsTypes.EMPTY.potion] = new Texture(Gdx.files.internal("resources/empty.png"));
+        textures[PotionsTypes.GREEN.potion] = new Texture(Gdx.files.internal("resources/green.png"));
+        textures[PotionsTypes.PURPLE.potion] = new Texture(Gdx.files.internal("resources/purple.png"));
+        textures[PotionsTypes.RED.potion] = new Texture(Gdx.files.internal("resources/red.png"));
+        textures[PotionsTypes.YELLOW.potion] = new Texture(Gdx.files.internal("resources/yellow.png"));
     }
 
 }
