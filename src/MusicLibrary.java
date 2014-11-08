842ede1e4fd945731fcbe89c1be8a22d7bea60b0
Jordanlw <jordanw2@gmail.com>
2014-11-08 22:38:20 +1030

    Renamed .wav to .ogg in MusicLibrary class, also added final modifier to fields & added printing of stacktrace on loading error

--- src/MusicLibrary.java
+++ src/MusicLibrary.java
@@ -10,17 +10,18 @@ import com.badlogic.gdx.audio.Sound;
  * To change this template use File | Settings | File Templates.
  */
 class MusicLibrary {
-    Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("resources/Invincible.mp3"));
-    Sound gunSound = Gdx.audio.newSound(Gdx.files.internal("resources/shotgun.wav"));
-    Sound potionSound = Gdx.audio.newSound(Gdx.files.internal("resources/healspell1.wav"));
-    Sound hurtSound = Gdx.audio.newSound(Gdx.files.internal("resources/slightscream-01.wav"));
-    Sound[] zombieSounds = new Sound[20];
+    final Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("resources/Invincible.ogg"));
+    final Sound gunSound = Gdx.audio.newSound(Gdx.files.internal("resources/shotgun.ogg"));
+    final Sound potionSound = Gdx.audio.newSound(Gdx.files.internal("resources/healspell1.ogg"));
+    final Sound hurtSound = Gdx.audio.newSound(Gdx.files.internal("resources/slightscream-01.ogg"));
+    final Sound[] zombieSounds = new Sound[21];
     public MusicLibrary() {
         for(int i = 1;i <= zombieSounds.length;i++) {
             try {
-                zombieSounds[i - 1] = Gdx.audio.newSound(Gdx.files.internal("resources/scaled-zombie" + i + ".wav"));
+                zombieSounds[i - 1] = Gdx.audio.newSound(Gdx.files.internal("resources/scaled-zombie" + i + ".ogg"));
             } catch (Exception e) {
-                System.out.println("Can't read zombie idle sound number:" + i);
+                e.printStackTrace();
+                //System.out.println("Can't read zombie idle sound number:" + i);
                 System.exit(1);
             }
         }
